90aed41ad636fa561441d28339419d3a6bd37722
Sylvie-B <unsolo@hotmail.fr>
2021-12-21 10:46:50 +0100

    losange

--- app.js
+++ app.js
@@ -22,3 +22,19 @@ $('.square').click(function (){
     }
 });
 
+$('.losange').click(function (){
+    let test = $(this).parent('section').siblings();
+    if ($(this).is('.painted')){
+        test.children().eq(2).addClass('painted');
+        test.children().eq(7).addClass('painted');
+        test.children().eq(12).addClass('painted');
+        test.children().eq(17).addClass('painted');
+    }
+    else{
+        test.children().eq(2).removeClass('painted');
+        test.children().eq(7).removeClass('painted');
+        test.children().eq(12).removeClass('painted');
+        test.children().eq(17).removeClass('painted');
+    }
+})
+
